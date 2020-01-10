61dc3e4046066490a34d19a3a7ccda1374685d3d
FSSDAWN <18280586233@163.com>
2020-01-10 22:11:38 +0800

    create framwork

---
+++ src/main/java/org/sicnuafcs/online_exam_platform/OnlineExamPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.sicnuafcs.online_exam_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sicnuafcs/online_exam_platform/OnlineExamPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sicnuafcs.online_exam_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
