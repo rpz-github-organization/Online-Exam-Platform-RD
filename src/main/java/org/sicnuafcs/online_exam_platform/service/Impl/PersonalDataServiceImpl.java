c1b3595f9377e40838557767414bb92c7d59f9a1
WangRey <983604371@qq.com>
2020-02-06 12:21:26 +0800

    “success Personal Data”

--- src/main/java/org/sicnuafcs/online_exam_platform/service/Impl/PersonalDataServiceImpl.java
+++ src/main/java/org/sicnuafcs/online_exam_platform/service/Impl/PersonalDataServiceImpl.java
@@ -21,14 +21,14 @@ import java.util.Optional;
 @Service
 public class PersonalDataServiceImpl implements PersonalDataService {
 
+
     @Resource
     TeacherRepository teacherRepository;
     @Resource
     StudentRepository studentRepository;
     @Autowired
     SendMailService sendMailService;
-    @Autowired
-    RegisterService registerService;
+
     @Override
     public Optional<Teacher> getTeacherData(String ID) {
         Optional<Teacher> teacherList = teacherRepository.findById(ID);
@@ -46,48 +46,26 @@ public class PersonalDataServiceImpl implements PersonalDataService {
     }
 
     @Override
-    public Teacher updateTeacherData(String ID,Map<String, Object> params) {
+    public Teacher updateTeacherPassword(String ID,Map<String, Object> params) {
         Teacher teacher = teacherRepository.findById(ID).get();
-//        log.info(String.valueOf(teacher));
         String newPassword = (String) params.get("newPassword"),
-                newEmail= (String) params.get("newEmail"),
                 newTelephone = (String) params.get("newTelephone"),
                 code = (String) params.get("code");
-
-
         //邮箱验证：验证码和邮箱是否一致；
         if (!sendMailService.verification(teacher.getEmail(), code)) {
             log.info("邮箱验证不一致");
             throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱验证失败");
         }
-
-        //这里只是检测是否重复
-
         if (newPassword.length() < 8) {
             log.info("密码位数过少");
             throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"密码位数过少");
         }
-//        if(teacher.getPassword().equals(newPassword)){
-//            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR, "和原始密码相同");
-//        }
-        //格式不正确前端检测
-        String result = newEmail.substring(newEmail.length()-13,newEmail.length());
-        if (!(newEmail.matches("[\\w\\.\\-]+@([\\w\\-]+\\.)+[\\w\\-]+") && result.equals("@sicnu.edu.cn"))) {
-            log.info("邮箱格式不正确");
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱格式不正确");
-        }
-        Optional<Teacher> emailList = teacherRepository.findByEmail(newEmail);
-        if (emailList.isPresent() && !emailList.get().getTea_id().equals(ID)) {
-            log.info("该邮箱已被注册");
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"该邮箱已被注册");
-        }
-//        Optional<Student> studentPhoneList = studentRepository.findByTelephone(newTelephone);
+//原始密码相同？
         Optional<Teacher> teacherPhoneList = teacherRepository.findByTelephone(newTelephone);
         if ( teacherPhoneList.isPresent() && !teacherPhoneList.get().getTea_id().equals(ID)) {
             log.info("该电话已被注册");
             throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"该电话已被注册");
         }
-
 //        try {
 //            teacher.setPassword(BCrypt.hashpw(newPassword, BCrypt.gensalt()));
 //            log.info("密码加密成功");
@@ -95,13 +73,9 @@ public class PersonalDataServiceImpl implements PersonalDataService {
 //            log.info("密码加密失败");
 //            throw new CustomException(CustomExceptionType.SYSTEM_ERROR,"密码加密失败");
 //        }
-
         teacher.setPassword(newPassword);
-        if(newEmail!=null)
-            teacher.setEmail(newEmail);
         if(newTelephone!=null)
             teacher.setTelephone(newTelephone);
-
         Teacher newTeacherData = teacherRepository.save(teacher);
             log.info(String.valueOf(newTeacherData));
         newTeacherData.setPassword(null);
@@ -110,10 +84,9 @@ public class PersonalDataServiceImpl implements PersonalDataService {
     }
 
     @Override
-    public Student updateStudentData(String ID, Map<String, Object> params) {
+    public Student updateStudentPassword(String ID, Map<String, Object> params) {
         Student student = studentRepository.findById(ID).get();
         String newPassword = (String) params.get("newPassword"),
-                newEmail= (String) params.get("newEmail"),
                 newTelephone = (String) params.get("newTelephone"),
                 code = (String) params.get("code");
         //邮箱验证：验证码和邮箱是否一致；
@@ -121,32 +94,15 @@ public class PersonalDataServiceImpl implements PersonalDataService {
             log.info("邮箱验证不一致");
             throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱验证失败");
         }
-
         if (newPassword.length() < 8) {
             log.info("密码位数过少");
             throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"密码位数过少");
         }
-        if(student.getPassword().equals(newPassword)){
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR, "和原始密码相同");
-        }
-
-        if (!(newEmail.matches("[\\w\\.\\-]+@([\\w\\-]+\\.)+[\\w\\-]+"))) {
-            log.info("邮箱格式不正确");
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱格式不正确");
-        }
-
-        Optional<Student> emailList = studentRepository.findByEmail(newEmail);
-        if (emailList.isPresent() && !emailList.get().getStu_id().equals(ID)) {
-            log.info("该邮箱已被注册");
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"该邮箱已被注册");
-        }
-//        Optional<Student> studentPhoneList = studentRepository.findByTelephone(newTelephone);
-        Optional<Student> studentPhoneList = studentRepository.findByEmail(newEmail);
+        Optional<Student> studentPhoneList = studentRepository.findByTelephone(newTelephone);
         if ( studentPhoneList.isPresent() && !studentPhoneList.get().getStu_id().equals(ID)) {
                 log.info("该电话已被注册");
                 throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"该电话已被注册");
         }
-
 //        try {
 //            student.setPassword(BCrypt.hashpw(newPassword, BCrypt.gensalt()));
 //            log.info("密码加密成功");
@@ -154,24 +110,16 @@ public class PersonalDataServiceImpl implements PersonalDataService {
 //            log.info("密码加密失败");
 //            throw new CustomException(CustomExceptionType.SYSTEM_ERROR,"密码加密失败");
 //        }
-
         student.setPassword(newPassword);
-        if(newEmail!=null){
-            student.setEmail(newEmail);
-        }
         if(newTelephone!=null){
             student.setTelephone(newTelephone);
         }
-
-
         Student newStudentData = studentRepository.save(student);
             log.info(String.valueOf(newStudentData));
         newStudentData.setPassword(null);
         newStudentData.setCode(null);
         return newStudentData;
-
     }
-
     @Override
     public Teacher editTeacherBaseData(String ID,Teacher newTeacherData) {
         Teacher teacher = teacherRepository.findById(ID).get();
@@ -195,27 +143,84 @@ public class PersonalDataServiceImpl implements PersonalDataService {
         student.setQq(newStudentData.getQq());
         student.setSex(newStudentData.getSex());
         student.setWeixin(newStudentData.getWeixin());
+
         Student student1 = studentRepository.save(student);
         student1.setPassword(null);
         student1.setCode(null);
         return student1;
     }
-
+    //更新教师邮箱
     @Override
-    public void checkStudentEmail(String ID) {
+    public Teacher updateTeacherEmail(String ID,Map<String, Object> params) {
+        //邮箱验证：验证码和邮箱是否一致；
+        String newEmail = (String) params.get("newEmail");
+        String code = (String) params.get("code");
+        if (!sendMailService.verification(newEmail, code)) {
+            log.info("邮箱验证不一致");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱验证失败");
+        }
+        //邮箱验证：是否唯一
+        Optional<Teacher> emailList = teacherRepository.findByEmail(newEmail);
+        if (emailList.isPresent() && !emailList.get().getTea_id().equals(ID)) {
+            log.info("该邮箱已被注册");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"该邮箱已被注册");
+        }
+        Teacher teacher = teacherRepository.findById(ID).get();
+        teacher.setEmail(newEmail);
+        Teacher newTeacher = teacherRepository.save(teacher);
+        newTeacher.setPassword(null);
+        newTeacher.setCode(null);
+        return newTeacher;
+
+    }
+    //更新学生邮箱
+    @Override
+    public Student updateStudentEmail(String ID,Map<String, Object> params) {
+        //邮箱验证：验证码和邮箱是否一致；
+        String newEmail = (String) params.get("newEmail");
+        String code = (String) params.get("code");
+        if (!sendMailService.verification(newEmail, code)) {
+            log.info("邮箱验证不一致");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱验证失败");
+        }
+        //邮箱验证：是否唯一
+        Optional<Student> emailList = studentRepository.findByEmail(newEmail);
+        if (emailList.isPresent() && !emailList.get().getStu_id().equals(ID)) {
+            log.info("该邮箱已被注册");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"该邮箱已被注册");
+        }
         Student student = studentRepository.findById(ID).get();
+        student.setEmail(newEmail);
+        Student newStudent =studentRepository.save(student);
+        newStudent.setPassword(null);
+        newStudent.setCode(null);
+        return newStudent;
+    }
+    //确保本人操作 或者 验证新的邮箱是否使用
+    @Override
+    public void checkStudentEmail(String email) {
+        if (!(email.matches("[\\w\\.\\-]+@([\\w\\-]+\\.)+[\\w\\-]+"))) {
+            log.info("邮箱格式不正确");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱格式不正确");
+        }
         try {
-            registerService.sendStudentEmail(student.getEmail());
+            sendMailService.sendEmail(email);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    //确保本人操作 或者 验证新的邮箱是否使用
     @Override
-    public void checkTeacherEmail(String ID) {
-        Teacher teacher = teacherRepository.findById(ID).get();
+    public void checkTeacherEmail(String email) {
+        //如果邮箱格式不正确（正则表达式验证）
+        String result = email.substring(email.length()-13,email.length());
+        if (!(email.matches("[\\w\\.\\-]+@([\\w\\-]+\\.)+[\\w\\-]+") && result.equals("@sicnu.edu.cn"))) {
+//            if (!(email.matches("[\\w\\.\\-]+@([\\w\\-]+\\.)+[\\w\\-]+"))) {
+            log.info("邮箱格式不正确");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"邮箱格式不正确");
+        }
         try {
-//            registerService.sendTeacherEmail(teacher.getEmail());
-            registerService.sendStudentEmail(teacher.getEmail());
+            sendMailService.sendEmail(email);
         } catch (Exception e) {
             e.printStackTrace();
         }
