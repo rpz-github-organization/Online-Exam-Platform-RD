95fda2452bf662b26704a451a67b823dcf42ebf4
swagere <1250539180@qq.com>
2020-02-07 21:30:21 +0800

    change Exam；

--- src/main/java/org/sicnuafcs/online_exam_platform/dao/ExamRepository.java
+++ src/main/java/org/sicnuafcs/online_exam_platform/dao/ExamRepository.java
@@ -1,8 +1,7 @@
 package org.sicnuafcs.online_exam_platform.dao;
 
 import org.sicnuafcs.online_exam_platform.model.Exam;
-import org.sicnuafcs.online_exam_platform.model.ExamPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExamRepository extends JpaRepository<Exam, ExamPk> {
+public interface ExamRepository extends JpaRepository<Exam, String> {
 }
--- src/main/java/org/sicnuafcs/online_exam_platform/model/Exam.java
+++ src/main/java/org/sicnuafcs/online_exam_platform/model/Exam.java
@@ -11,40 +11,27 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@IdClass(ExamPk.class)
 @Table(name = "exam")
 public class Exam {
     public static enum ProgressStatus {
         ING,
         DONE
     }
-    @Id
+
     @Column
     @NotBlank(message = "课程号不能为空")
     private String co_id; //课程id
 
-    @Id
     @Column
     @NotBlank(message = "工号不能为空")
     private String tea_id; //教师id
 
     @Id
     @Column
-    private int exam_id;
+    private long exam_id;
 
     private ProgressStatus progress_status; //状态
     private String name;
     private String begin_time;
     private int last_time; //持续时间
-    @Column(columnDefinition = "text")
-    private String single_list; //单选题列表
-    @Column(columnDefinition = "text")
-    private String fillintheblank_list; //填空题列表
-    @Column(columnDefinition = "text")
-    private String judge_list; //判断题列表
-    @Column(columnDefinition = "text")
-    private String discussion_list; //问答题列表
-    @Column(columnDefinition = "text")
-    private String program_list; //编程题列表
-
 }
