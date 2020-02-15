08edd2fbb1dd9326111317ee040d618f57843537
lynneyli <lynneyli@tencent.com>
2020-02-15 20:17:00 +0800

    fix

--- src/main/java/org/sicnuafcs/online_exam_platform/config/LoginConfig.java
+++ src/main/java/org/sicnuafcs/online_exam_platform/config/LoginConfig.java
@@ -19,7 +19,6 @@ public class LoginConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/register/student")
                 .excludePathPatterns("/register/teacher")
                 .excludePathPatterns("/register/email/student")
-                .excludePathPatterns("/register/email/teacher")
-                .excludePathPatterns("/exam//stuExamInfo");
+                .excludePathPatterns("/register/email/teacher");
     }
 }
